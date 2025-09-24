4415cf4ebae5c7d5dc4082d20db395a5a7848d58
wikadii <wikadi2000@gmail.com>
2025-09-24 11:17:57 +0200

    warsztat

--- src/Rook.java
+++ src/Rook.java
@@ -8,21 +8,7 @@ public class Rook extends Piece{
     }
     @Override
     public Boolean validateMove(int destinationCol, int destinationRow, ChessPanel board) {
-        int colChange = destinationCol - col;
-        int rowChange = destinationRow - row;
-        int stepCol = Integer.signum(colChange);
-        int stepRow = Integer.signum(rowChange);
-        int steps = Math.max(Math.abs(colChange), Math.abs(rowChange));
-
-        if (colChange != 0 && rowChange != 0) return false;
-
-        for (int i = 1; i < steps; i++) {
-            int currentCol = col + i * stepCol;
-            int currentRow = row + i * stepRow;
-            if (board.getPieceAt(currentCol, currentRow) != null) return false;
-        }
-
-        return verifyTarget(board, destinationCol, destinationRow);
+        return false;
     }
 
 }
